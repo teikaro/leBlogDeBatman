e0b9c067b472983b7bccbaa6f62bb06382f83819
Corentin <a@a.a>
2021-10-08 14:32:26 +0200

    Création du système d'authentification

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,11 +6,14 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(fields={"email"}, message="Cette adresse email est déjà utilisée par un autre compte")
+ * @UniqueEntity(fields={"pseudonym"}, message="Ce pseudonym est déjà utilisé par un autre compte")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
